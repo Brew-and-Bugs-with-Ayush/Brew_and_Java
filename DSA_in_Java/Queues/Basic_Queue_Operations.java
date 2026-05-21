1f9fe3fc689104d22b7b72ede371da71618c4911
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-21 22:50:58 +0530

    add comments to queue operation file

--- DSA_in_Java/Queues/Basic_Queue_Operations.java
+++ DSA_in_Java/Queues/Basic_Queue_Operations.java
@@ -5,13 +5,19 @@ import java.util.*;
 public class Basic_Queue_Operations {
     public static void main(String[] args) {
 
+        // Queue follows FIFO (First In First Out)
         Queue<Integer> q = new LinkedList<>();
 
+        // Add elements to the rear of the queue
         q.add(10); q.add(20); q.add(30); q.add(40); q.add(50);
 
         System.out.println(q);
+
+        // Removes the front element from the queue
         q.poll();
         System.out.println(q);
+
+        // Returns the front element without removing it
         System.out.println(q.peek());
         }
 }
