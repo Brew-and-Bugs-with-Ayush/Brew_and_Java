2d2c3d77506ed9549aee35184cbc755dda436c86
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-05-21 22:45:25 +0530

    add comments and description

--- DSA_in_Java/Recursion/PowerOf_A_RaisedTo_B.java
+++ DSA_in_Java/Recursion/PowerOf_A_RaisedTo_B.java
@@ -7,18 +7,29 @@ public class PowerOf_A_RaisedTo_B {
         int ans = power2(a , b);
         System.out.println(ans);
     }
+
+    // Normal recursion: multiplies 'a' b times -> O(b)
     public static int power1(int a , int b){
+
+        // Base case: any number raised to power 0 is 1
         if(b == 0) return 1;
 
         return a * power1(a , b-1);
     }
 
+    // Optimized recursion using Divide and Conquer -> O(log b)
     public static int power2(int a , int b){
+
+        // Base case
         if(b == 0) return 1;
 
+        // Recursively calculate a^(b/2)
         int call = power2(a , b / 2);
 
+        // If exponent is even: a^b = (a^(b/2))²
         if(b % 2 == 0) return call * call;
+
+            // If exponent is odd: a^b = a × (a^(b/2))²
         else return a * call * call;
 
     }
